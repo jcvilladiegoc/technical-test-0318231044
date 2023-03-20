0e9c0381ce122358f96b898391b2d21a79ab9554
Jairo Cesar Villadiego Contreras <jcvilladiegoc@gmail.com>
2023-03-20 00:28:34 -0500

    #4 #comment The project structure for the rest api was defined

---
+++ api/core/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+    define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+    // When starting the project, the connections to the database, models, services and controllers are imported
+
+    // Controllers
+    require_once PROJECT_ROOT_PATH . "controllers/BaseController.php";
+
+?>
